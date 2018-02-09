f48088da269cb1a0b6983dc417b07d6f330e9063
Nasib <nmansou4@vols.utk.edu>
2018-02-09 12:52:54 -0500

    Add hello.py

--- website/hello.py
+++ website/hello.py
@@ -7,12 +7,14 @@ functionCalls = sys.argv[2]
 #Bins
 #Probs
 
-
+'''
 if "powerLaw" in functionCalls:
 	#Function call to powerLaw
 if "threshold" in functionCalls:
 	#Function call threshold
 if "logisticThreshold" in functionCalls:
 	#Function call logisticThreshold
+'''
 
+print ("hello")
 #print "Hello %s" % who + " " + who2
