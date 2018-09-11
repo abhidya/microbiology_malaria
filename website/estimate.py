7c03fe2a055f094b635f639f7d7532a6c4c33d8a
Manny Bhidya <abhidya@vols.utk.edu>
2018-09-11 09:18:17 -0400

    made all three graphs work

--- website/estimate.py
+++ website/estimate.py
@@ -2,137 +2,126 @@ import sys
 from math import exp
 from scipy.stats import norm
 from scipy.stats import uniform
-from numpy import log10, power, arange,array, zeros, random, abs, set_printoptions, inf
+from numpy import log10, power, arange, array, zeros, random, abs, set_printoptions, inf
 import random as rand
 import matplotlib.pyplot as plt
 
 set_printoptions(threshold=inf)
 
-
 # Defining 3 functions fitted to experimental data
-powerlaw = lambda v: 1 - exp(-0.0037777154*v**0.41129882)
-threshold = lambda v: 0.06639757 if v<20166.6408644 else 0.352561152
-logistic_threshold = lambda v: (0.04131329044/(0.04131329044 + exp(-0.0001102641*v))) if v<20737.338791655 else (0.04131329044/(0.04131329044 + exp(-0.0001102641*20737.338791655 - 3.25839158*(v - 20737.338791655))))  
+powerlaw = lambda v: 1 - exp(-0.0037777154 * v ** 0.41129882)
+threshold = lambda v: 0.06639757 if v < 20166.6408644 else 0.352561152
+logistic_threshold = lambda v: (0.04131329044 / (0.04131329044 + exp(-0.0001102641 * v))) if v < 20737.338791655 else (
+            0.04131329044 / (0.04131329044 + exp(-0.0001102641 * 20737.338791655 - 3.25839158 * (v - 20737.338791655))))
 
-
-#Constructing data on infectivity from website
+# Constructing data on infectivity from website
 args = sys.argv[1]
-arglist = args.split()        #turn the args string into list items
+arglist = args.split()  # turn the args string into list items
 
+numbins = int(arglist[0])  # number of bins
 
-numbins = int(arglist[0])        #number of bins
+numfunctions = int(arglist[1])  # number of functions
 
+numsamples = int(arglist[2])  # numver of samples
 
-numfunctions = int(arglist[1])   #number of functions
-
-numsamples = int(arglist[2])     #numver of samples
-
-
-
-cutoffs = []    
+cutoffs = []
 probs = []
 intervals = []
 lengths = []
 
-for i in range(3, 3*numbins, 3): 
-  intervals.append([float(arglist[i]), float(arglist[i+1])])
-  cutoffs.append(float(arglist[i+1]))
-  probs.append(float(arglist[i+2])*0.01)
-  lengths.append(float(arglist[i+1])-float(arglist[i]))
+for i in range(3, 3 * numbins, 3):
+    intervals.append([float(arglist[i]), float(arglist[i + 1])])
+    cutoffs.append(float(arglist[i + 1]))
+    probs.append(float(arglist[i + 2]) * 0.01)
+    lengths.append(float(arglist[i + 1]) - float(arglist[i]))
 
-#print(cutoffs)
-#print(probs)
-#print(intervals)
-#print(lengths)
+# print(cutoffs)
+# print(probs)
+# print(intervals)
+# print(lengths)
 
 # Cache for cdfProbSpz
-cdf_cache = zeros(int(max(cutoffs))+1)
+cdf_cache = zeros(int(max(cutoffs)) + 1)
 cdf_cache[0] = 0
 
 
 # Defining probability ditribution
 def ProbSpz(x):
-  for i in range(numbins):
-    if (x>=intervals[i][0] and x<=intervals[i][1]):
-      return probs[i], lengths[i]
-      break
+    for i in range(numbins):
+        if (x >= intervals[i][0] and x <= intervals[i][1]):
+            return probs[i], lengths[i]
+            break
 
 
 # Defining probability density function
 def cdfProbSpz(x):
-  if (cdf_cache[x]!=0):
+    if (cdf_cache[x] != 0):
+        return cdf_cache[x]
+    else:
+        prob, length = ProbSpz(x)
+        cdf_cache[x] = cdf_cache[x - 1] + (prob / length)
     return cdf_cache[x]
-  else:
-    prob, length = ProbSpz(x)
-    cdf_cache[x] = cdf_cache[x-1]+(prob/length)
-  return cdf_cache[x]
 
 
 # Defining a sample from the distribution
 def mosquito_sample(val_array):
-  sample = rand.random()
-  
-  #return the index of closest value of sample in val_array
-  index = (abs(val_array-sample)).argmin()
-  
-  return index+1
+    sample = rand.random()
+
+    # return the index of closest value of sample in val_array
+    index = (abs(val_array - sample)).argmin()
+
+    return index + 1
 
 
 def main():
-  max_value = int(max(cutoffs))
-#  numsamples = 10000
-  val_array = zeros(max_value)
-  sample_array = zeros(numsamples)
+    max_value = int(max(cutoffs))
+    val_array = zeros(max_value)
+    sample_array = zeros(numsamples)
 
-  for i in range(max_value):
-    val_array[i] = cdfProbSpz(i+1)
+    for i in range(max_value):
+        val_array[i] = cdfProbSpz(i + 1)
+        powerlaw_probs = zeros(numsamples)
+        threshold_probs = zeros(numsamples)
+        logisticthreshold_probs = zeros(numsamples)
 
-  if("powerLaw" in arglist):
-    powerlaw_probs = zeros(numsamples)
-  if("threshold" in arglist):
-    threshold_probs = zeros(numsamples)
-  if("logisticThreshold" in arglist):
-    logisticthreshold_probs = zeros(numsamples)
+    for i in range(numsamples):
+        sample_array[i] = mosquito_sample(val_array)
+        powerlaw_probs[i] = powerlaw(sample_array[i])
+        threshold_probs[i] = threshold(sample_array[i])
+        logisticthreshold_probs[i] = logistic_threshold(sample_array[i])
 
 
-  for i in range(numsamples):
-    sample_array[i] = mosquito_sample(val_array)
-    if("powerLaw" in arglist):
-      powerlaw_probs[i] = powerlaw(sample_array[i])
-    if("threshold" in arglist):
-      threshold_probs[i] = threshold(sample_array[i])
-    if("logisticThreshold" in arglist):
-      logisticthreshold_probs[i] = logistic_threshold(sample_array[i])
+    # print("powerlaw_probs,")
 
-  if("powerLaw" in arglist):
-#    print(powerlaw_probs)
     for i in powerlaw_probs:
-      print(i) 
-  if("threshold" in arglist):
-#    print(threshold_probs)
+        print(i)
+    print(" , ")
+    # print("thresholds_probs,")
+
     for i in threshold_probs:
-      print(i)
-  if("logisticThreshold" in arglist):
-#    print(logisticthreshold_probs)
+        print(i)
+    print(" , ")
+
+    # print("logtresh_probs,")
+
+
     for i in logisticthreshold_probs:
-      print(i)
-
-#  plt.hist(log10(sample_array), 10)
-#  plt.show()
-
-  plt.hist(powerlaw_probs, normed = True)
-  plt.show()
-  plt.hist(threshold_probs, normed = True)
-  plt.show()
-  plt.hist(logisticthreshold_probs, normed = True)
-  plt.show()
-     
-
-  print ("\n")
- 
+        print(i)
+    print(" , ")
 
 
+     # plt.hist(log10(sample_array), 10)
+     # plt.show()
 
+
+    plt.hist(powerlaw_probs, normed=True)
+    plt.show()
+    plt.hist(threshold_probs, normed=True)
+    plt.show()
+    plt.hist(logisticthreshold_probs, normed=True)
+    plt.show()
+
+    print("\n")
 
 if __name__ == "__main__":
-  main()
+    main()
--- website/process.php
+++ website/process.php
@@ -45,11 +45,20 @@ echo("<br>");
 
 ###END function parse information###
 
-$page = shell_exec("/usr/bin/python3 /var/www/html/website/estimate.py '" .$args."'"); #Nasib Ubuntu
-$page = shell_exec("/usr/bin/python3 /var/www/html/website/estimate.py '" .$args."'"); #Mark Windows
-$page = shell_exec("/usr/bin/python3 /var/www/html/website/estimate.py '" .$args."'"); #Kristina Mac
-$page = shell_exec("/usr/bin/python3 /var/www/html/website/estimate.py '" .$args."'"); #Manny Ubuntu
-echo($page);
+
+#$page = shell_exec("/usr/bin/python3 /var/www/html/website/estimate.py '" .$args."'"); #Nasib Ubuntu
+#$page = shell_exec("/usr/bin/python3 /var/www/html/website/estimate.py '" .$args."'"); #Mark Windows
+#$page = shell_exec("/usr/bin/python3 /var/www/html/website/estimate.py '" .$args."'"); #Kristina Mac
+$page = shell_exec("/home/manny/anaconda3/bin/python3 /var/www/html/microbiology_malaria/website/estimate.py '" .$args."'"); #Manny Ubuntu
+// echo($page)
+$models_ar = explode(', ', $page);
+// echo($models_ar[0])
+
+// $handle = popen("/home/manny/anaconda3/bin/python3 /var/www/html/microbiology_malaria/website/estimate.py 2>&1 '" .$args."'" , 'r');
+// $output = fread($handle, 1024);
+// var_dump($output);
+// pclose($handle);
+// echo($handle)
 
 ?>
 
@@ -61,11 +70,12 @@ echo($page);
 </head>
 
 <body>
+    <h1> powerlaw_probs <h1>
     <!-- Plotly chart will be drawn inside this DIV -->
-    <div id="myDiv"></div>
+    <div id="powerlaw_probs"></div>
     <script>
     /* JAVASCRIPT CODE GOES HERE */
-    var data = <?php echo json_encode($page); ?>;
+    var data = <?php echo json_encode($models_ar[0]); ?>;
     var x= data.split("\n");
 
     var trace = {
@@ -73,6 +83,43 @@ echo($page);
         type: 'histogram',
     };
     var data = [trace];
-    Plotly.newPlot('myDiv', data);
+    Plotly.newPlot('powerlaw_probs', data);
+</script>
+
+
+    <!-- Plotly chart will be drawn inside this DIV -->
+    <h1> threshold_probs <h1>
+
+    <div id="threshold_probs"></div>
+    <script>
+    /* JAVASCRIPT CODE GOES HERE */
+    var data = <?php echo json_encode($models_ar[1]); ?>;
+    var x= data.split("\n");
+
+    var trace = {
+        x: x,
+        type: 'histogram',
+    };
+    var data = [trace];
+    Plotly.newPlot('threshold_probs', data);
+</script>
+
+
+
+    <!-- Plotly chart will be drawn inside this DIV -->
+    <h1> logisticthreshold_probs <h1>
+
+    <div id="logisticthreshold_probs"></div>
+    <script>
+    /* JAVASCRIPT CODE GOES HERE */
+    var data = <?php echo json_encode($models_ar[2]); ?>;
+    var x= data.split("\n");
+
+    var trace = {
+        x: x,
+        type: 'histogram',
+    };
+    var data = [trace];
+    Plotly.newPlot('logisticthreshold_probs', data);
 </script>
 </body>
