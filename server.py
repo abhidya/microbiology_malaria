3cf5d0fe8d83028bc42ebf95b8fa9cb7ed16b3c7
Manny Bhidya <abhidya@vols.utk.edu>
2018-10-02 23:41:44 -0400

    Added Data Storage

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 from threading import Lock
-
+import json
 from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
-
+import datetime
 from compute import compute
-
 async_mode = None
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -20,9 +21,11 @@ thread_lock = Lock()
 def index():
     return render_template('index.html')
 
+
 @app.route('/example.html')
 def example():
     return render_template('example.html')
+
 
 @app.route('/custom.html')
 def custom():
@@ -36,8 +39,16 @@ def new_game():
     functionLaw = request.form.getlist('functionLaw[]')
     binsStart = request.form.getlist('binsStart[]')
     binsEnd = request.form.getlist('binsEnd[]')
+    location = request.form['location_form']
+    species = request.form['species_form']
     data = compute(functionLaw, size, probs, binsStart, binsEnd)
-    # print(data)
+    print(functionLaw, size, probs, binsStart, binsEnd, location, species)
+    dict = {"location": location, "species": species, "size": size, "binStart": binsStart, "binEnd": binsEnd,
+            "Probability": probs, "Models": functionLaw, "date": datetime.datetime.utcnow()}
+    r = json.dumps(dict, indent=4, sort_keys=True, default=str)
+    loaded_r = json.loads(r)
+    print(loaded_r)
+    mongo.db.data.insert_one(loaded_r)
     return jsonify(data), 200
 
 
