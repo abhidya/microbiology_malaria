63d6ec9e2348574d147eb0d3d0a928a10e7b4b85
Manny Bhidya <abhidya@vols.utk.edu>
2018-10-31 13:48:25 -0400

    Updated Data_pop to inset location and species with updated site

--- data_pop.py
+++ data_pop.py
@@ -9,10 +9,9 @@ url = "http://127.0.0.1:5000/example.html"
 species = ["Culex", "Anopheles", "Aedes", 'Abraedes', 'Aedes', 'Alanstonea', 'Albuginosus', ]
 locations = ["ABW", "AFG", "AGO", "AIA", ]
 
-functionLaw = ['powerLaw','threshold', 'logisticThreshold']
+functionLaw = ['powerLaw', 'threshold', 'logisticThreshold']
 
 requests.packages.urllib3.disable_warnings()
-
 
 for i in tqdm.tqdm(range(1000)):
     c = Session()
@@ -21,17 +20,18 @@ for i in tqdm.tqdm(range(1000)):
     browser = RoboBrowser(session=c, parser='html.parser')
 
     browser.open(url)
+    # # print(browser.parsed)
     form = browser.get_form()
-    # print(form)
 
-
-    form['location_form'] = random.choice(locations)
-    form['species_form'] = random.choice(species)
+    form['location'] = random.choice(locations)
+    form['species'] = random.choice(species)
     form['size'] = 1
     form['functionLaw[]'] = random.choice(functionLaw)
+
     response = browser.submit_form(form)
 
-
+    #
+    # browser.open('http://127.0.0.1:5000/compute', method='post', data=form)
+    #
     # print(response)
     # print(browser.parsed)
-
