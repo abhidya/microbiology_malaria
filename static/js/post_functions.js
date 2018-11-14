004ee5eb82798433526337102ef1cf877f4c55eb
kristinabridgwater <kbridgwa@vols.utk.edu>
2018-11-14 00:25:53 -0500

    change order of graphs on PDF

--- static/js/post_functions.js
+++ static/js/post_functions.js
@@ -157,7 +157,7 @@ function all_graphs(a, b, c) {
         },
     };
 
-    var alldata = [trace1, trace2, trace3];
+    var alldata = [trace1, trace3, trace2];
     var layout = {
         showlegend: true,
         legend: {
